8cd2c4fd7e0f9980285fea6b68d1594b3b023cd0
Nate Hudson <nate@shift.io>
2018-05-18 15:36:24 -0400

    travis test

---
+++ make-steps/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+#docker build -t travis-ci-build-stages-demo .
+#docker images
+#docker tag travis-ci-build-stages-demo $DOCKER_USERNAME/travis-ci-build-stages-demo
+#docker push $DOCKER_USERNAME/travis-ci-build-stages-demo
