935bc5feb8a3537bb3cc1d883422db7963ed16eb
Nate Hudson <nate@shift.io>
2018-05-25 17:39:34 -0400

    cpu load test

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var sleep = require('sleep');
+var crypto = require('crypto');
 const express = require('express');
 
 // Constants
@@ -13,11 +15,19 @@ const app = express();
 
 //console.log(process.env);
 
+function spincpu() {
+  var rando = crypto.randomBytes(500000).toString('hex');
+  console.log('sleeping 5');
+  sleep.sleep(5);
+}
+
 app.get('/', (req, res) => {
+  spincpu();
   res.send('<img src="https://shift.io/images/logo.svg"><h1>CI/CD PoC #1</h1>\n<br \><pre>sha:'+GIT_SHA+'</pre><pre>branch:'+GIT_BRANCH+'</pre>');
 });
 
 app.get('/healthCheck', (req, res) => {
+  spincpu();
   res.send('healthy\n');
 });
 
